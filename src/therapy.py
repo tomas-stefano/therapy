1799b316fea2cecdec774eaa3fc3d8dd7a0d50d7
Tomas D'Stefano <tomas_stefano@successoft.com>
2014-02-21 14:40:50 -0300

    Making a unfinished draft about the classes for this package

---
+++ src/therapy.py
@@ -0,0 +1,62 @@
+import os
+import sys
+import json
+import time
+from optparse import OptionParser
+
+class Statistics(object):
+  """docstring for Statistics"""
+  def __init__(self):
+    """docstring for __init__"""
+    pass
+
+  def retrieve(self):
+    """docstring for retrieve"""
+    pass
+
+class CPUStatistics(object):
+  """docstring for CPUStatistics"""
+  def __init__(self):
+    """docstring for __init__"""
+    pass
+
+  def retrieve(self):
+    """docstring for retrieve"""
+    pass
+
+class MemStatistics(object):
+  """docstring for MemStatistics"""
+  def __init__(self):
+    """docstring for __init__"""
+    pass
+
+  def retrieve(self):
+    """docstring for retrieve"""
+    pass
+
+class NetworkStatistics(object):
+  def __init__(self):
+    """docstring for __init__"""
+    pass
+
+  def retrieve(self):
+    """docstring for retrieve"""
+    pass
+
+class UptimeStatistics(object):
+  def __init__(self):
+    """docstring for __init__"""
+    pass
+
+  def retrieve(self):
+    """docstring for retrieve"""
+    pass
+
+class DiskStatistics(object):
+  def __init__(self):
+    """docstring for __init__"""
+    pass
+
+  def retrieve(self):
+    """docstring for retrieve"""
+    pass
